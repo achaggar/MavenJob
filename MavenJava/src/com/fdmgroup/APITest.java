f21dce27c69759ed1a0cdde6b87ec12c144f20f3
Amrit Chaggar <achaggar07@gmail.com>
2019-04-16 10:47:43 -0400

    adding first project through eclipse

---
+++ MavenJava/src/com/fdmgroup/APITest.java
@@ -0,0 +1,13 @@
+package com.fdmgroup;
+
+import org.junit.Test;
+
+public class APITest {
+		
+	@Test
+		public void ApTest() {
+		System.out.println("This is API Test");
+		}
+		
+	
+}
---
+++ MavenJava/src/com/fdmgroup/SeleniumTest.java
@@ -0,0 +1,18 @@
+package com.fdmgroup;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+
+	@Test
+	
+	public void postJira() {
+		System.out.println("post jira");
+
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("delete twitter");
+	}
+}
